799d2e52b6978c0415d60df8a8ec3395f3577ae6
Adele Noronha <adele.c.noronha@gmail.com>
2021-06-19 02:00:04 +0000

    add ejs engine

--- Ex_Files_CSS_EssT/css-portfolio/app.js
+++ Ex_Files_CSS_EssT/css-portfolio/app.js
@@ -2,9 +2,11 @@ const express = require("express");
 const app = express();
 const PORT = 8080;
 
-app.get("/", (req, res) => {
-  res.render("index");
-});
+app.set("view engine", "ejs");
+
+// app.get("/", (req, res) => {
+//   res.render("index");
+// });
 
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}`);
