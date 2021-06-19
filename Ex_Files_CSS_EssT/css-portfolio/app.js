75b0b53ef90e5d8248b38c1bf923e2eba9bd83f9
Adele Noronha <adele.c.noronha@gmail.com>
2021-06-19 03:29:47 +0000

    add src file, set views and static files in app.js

--- Ex_Files_CSS_EssT/css-portfolio/app.js
+++ Ex_Files_CSS_EssT/css-portfolio/app.js
@@ -2,7 +2,15 @@ const express = require("express");
 const app = express();
 const PORT = 8080;
 
-app.set("view engine", "ejs");
+// Static Files
+app.use(express.static('public'))
+app.use('/css', express.static(__dirname + 'public/css'))
+app.use('/js', express.static(__dirname + 'public/js'))
+app.use('/images', express.static(__dirname + 'public/images'))
+
+// Set Views
+app.set('views', './src/views');
+app.set('view engine', 'ejs');
 
 app.get("/", (req, res) => {
   res.render("portfolio_index");
